1b176371db161c5ba84f200db54e5b9c64fc9bb3
denis.morozov <denis.morozov>
2022-01-26 19:18:55 +0000

    starting

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,6 +1,15 @@
 pipeline {
-    agent { docker { image 'maven:3.8.4-openjdk-11-slim' } }
+    agent {
+        docker {
+            image 'maven:3.8.4-openjdk-11-slim'
+        }
+    }
     stages {
+        stage('Initialize') {
+            def dockerHome = tool 'myDocker'
+            env.PATH = "${dockerHome}/bin:${env.PATH}"
+        }
+
         stage('build') {
             steps {
                 sh 'mvn --version'
