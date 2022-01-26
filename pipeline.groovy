e312782671873d3f0a75405284d8a6d8598d1d0d
denis.morozov <denis.morozov>
2022-01-26 20:37:02 +0000

    starting

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,8 +1,11 @@
 pipeline {
-    agent {
-        docker { image 'node:16.13.1-alpine' }
-    }
+    agent none
+    
     stages {
+        agent {
+            docker { image 'node:16.13.1-alpine' }
+        }
+
         stage('Test') {
             steps {
                 sh 'node --version'
