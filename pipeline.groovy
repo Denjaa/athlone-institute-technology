af22ba1cb59825d23c3b9e010097df010989cb35
denis.morozov <denis.morozov>
2022-01-26 20:40:29 +0000

    starting

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,6 +2,8 @@ pipeline {
   agent none
   stages {
     stage('Back-end') {
+        def dockerHome = tool 'myDocker'
+        env.PATH = "${dockerHome}/bin:${env.PATH}"
       agent {
         docker { image 'maven:3.8.1-adoptopenjdk-11' }
       }
