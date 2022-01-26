17e9ea59692e408e143a3b2ce9b98e54819fb50c
denis.morozov <denis.morozov>
2022-01-26 19:39:22 +0000

    starting

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,6 @@
 pipeline {
     stages {
-        stage("Fetch repository") {
-            steps {
-                git 'https://github.com/Denjaa/athlone-institute-technology.git'
-            }
-        }
+        
         stage('Run test') {
             steps {
                 script {
@@ -15,27 +11,4 @@ pipeline {
             }
         }
     }
-}
-//    post {
-//        always {
-//            echo 'Test run completed'
-//            cucumber buildStatus: 'UNSTABLE', failedFeaturesNumber: 999, failedScenariosNumber: 999, failedStepsNumber: 3, fileIncludePattern: '**/*.json', skippedStepsNumber: 999
-//        }
-//        success {
-//            echo 'Successfully!'
-//        }
-//        failure {
-//            echo 'Failed!'
-//        }
-//        unstable {
-//            echo 'This will run only if the run was marked as unstable'
-//        }
-//        changed {
-//            echo 'This will run only if the state of the Pipeline has changed'
-//            echo 'For example, if the Pipeline was previously failing but is now successful'
-//        }
-//    }
-//    options {
-//        timeout(time: 60, unit: 'MINUTES')
-//    }
-//}
+}}
